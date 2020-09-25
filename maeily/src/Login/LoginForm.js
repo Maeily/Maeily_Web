7eae1dc570ff7ef4446875990054667888c2e976
Hong-JunHyeok <edb1631@naver.com>
2020-09-25 13:57:25 +0900

    [UPDATE]
    로그인 폼 디자인 개선

--- maeily/src/Login/LoginForm.js
+++ maeily/src/Login/LoginForm.js
@@ -7,18 +7,17 @@ function LoginForm() {
             <div className="title">매일리</div>
             <div className="login_form_wrap">
                 <form action="">
-                    <div>
-                        <input
-                            type="id"
-                            className="form_input"
-                            placeholder="아이디를 입력해주세요"
-                        />
-                        <input
-                            type="password"
-                            className="form_input"
-                            placeholder="비밀번호를 입력해주세요"
-                        />
-                    </div>
+                    <input
+                        type="id"
+                        className="form_input"
+                        placeholder="아이디를 입력해주세요"
+                    />
+                    <input
+                        type="password"
+                        className="form_input"
+                        placeholder="비밀번호를 입력해주세요"
+                    />
+
                     <input
                         type="button"
                         className="form_input input_submit"
