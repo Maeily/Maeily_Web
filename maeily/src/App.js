6752cfa5d1c764b6af4a79d211232158898ea2b4
Hong-JunHyeok <edb1631@naver.com>
2020-09-29 23:04:59 +0900

    [UPDATE]
    초기 셋팅

--- maeily/src/App.js
+++ maeily/src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
-import SignInForm from "./components/signIn/SignInForm";
-import SignUpForm from "./components/signUp/SignUpForm";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <SignUpForm></SignUpForm>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
