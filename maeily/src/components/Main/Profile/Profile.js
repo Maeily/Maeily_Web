21be9a22a1ea5a9a5bad4999aaf06292be333e85
Hong-JunHyeok <edb1631@naver.com>
2020-10-01 16:43:29 +0900

    [UPDATE]
    프로필 카드 1차 완성

--- maeily/src/components/Main/Profile/Profile.js
+++ maeily/src/components/Main/Profile/Profile.js
@@ -4,21 +4,12 @@ import "./Profile.scss";
 function Profile() {
     return (
         <div className="profile">
-            <img
-                src="../../../assets/logo.png"
-                alt="프로필 사진입니다."
-                className="profile_picture"
-            />
-
-            <div className="profile_info">
-                <div className="profile_info_name">홍준혁</div>
-                <div className="profile_info_grade">1학년 2반</div>
-            </div>
-            <div>
-                <button className="profile_logOut">로그아웃</button>
-            </div>
-            <div>
-                <button className="profile_setting">설정</button>
+            <img src="" alt="프로필" className="profile_img" />
+            <div className="profile_name">홍준혁</div>
+            <div className="profile_grade">1학년 2반</div>
+            <div className="profile_button">
+                <button className="profile_button_setting">설정</button>
+                <button className="profile_button_logOut">로그아웃</button>
             </div>
         </div>
     );
