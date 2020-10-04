327b56ed8fa2a66b0e611a8d39933c5f5e9ed1b7
Hong-JunHyeok <edb1631@naver.com>
2020-10-04 18:33:17 +0900

    [UPDATE]

---
+++ maeily/src/components/Main/Channel/Channel.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Channel.scss";
+function Channel() {
+    return (
+        <div className="Channel">
+            <div className="Channel_title">채널</div>
+            <div className="Channel_channels">
+                <div className="Channel_channels_channel"></div>
+                <div className="Channel_channels_channel"></div>
+                <div className="Channel_channels_channel"></div>
+                <div className="Channel_channels_channel"></div>
+                <div className="Channel_channels_channel"></div>
+                <div className="Channel_channels_channel"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Channel;
--- maeily/src/pages/MainPage.js
+++ maeily/src/pages/MainPage.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Profile from "../components/Main/Profile/Profile";
+import Cafeteria from "../components/Main/Cafeteria/Cafeteria";
+import Channel from "../components/Main/Channel/Channel";
 
 function MainPage() {
-    return <Profile></Profile>;
+    return (
+        <>
+            <Profile></Profile>
+            <Cafeteria></Cafeteria>
+            <Channel></Channel>
+        </>
+    );
 }
 
 export default MainPage;
