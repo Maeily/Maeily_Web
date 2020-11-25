a5906738a394fc539a26809ac3ce4b80e20e2894
HongJunHyeok <edb1631@naver.com>
2020-11-25 18:03:36 +0900

    로그인 회원가입 성공

--- maeily/src/components/Auth/Join/Join.jsx
+++ maeily/src/components/Auth/Join/Join.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 import "./Join.scss";
 
-export default function Join() {
+export default function Join({ state, onChange, onSubmit }) {
+    const { email, intro, name, password, chkPwd } = state;
     return (
         <div className="JoinDiv">
             <h1 className="JoinDiv_title">회원가입</h1>
             <div className="JoinDiv_form">
-                <input type="text" placeholder="이름을 입력해주세요" />
-                <input type="email" placeholder="이메일을 입력해주세요" />
-                <input type="password" placeholder="비밀번호를 입력해주세요" />
-                <input type="password" placeholder="비밀번호 다시!" />
-                <textarea placeholder="나를 소개하는 한마디!" />
-                <button>회원가입</button>
+                <input
+                    type="text"
+                    placeholder="이름을 입력해주세요"
+                    name="name"
+                    value={name}
+                    onChange={onChange}
+                />
+                <input
+                    type="email"
+                    placeholder="이메일을 입력해주세요"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                />
+                <input
+                    type="password"
+                    placeholder="비밀번호를 입력해주세요"
+                    name="password"
+                    value={password}
+                    onChange={onChange}
+                />
+                <input
+                    type="password"
+                    placeholder="비밀번호 다시!"
+                    name="chkPwd"
+                    value={chkPwd}
+                    onChange={onChange}
+                />
+                <textarea
+                    placeholder="나를 소개하는 한마디!"
+                    name="intro"
+                    value={intro}
+                    onChange={onChange}
+                />
+                <button onClick={onSubmit}>회원가입</button>
             </div>
         </div>
     );
--- maeily/src/containers/Auth/Join/JoinContainer.jsx
+++ maeily/src/containers/Auth/Join/JoinContainer.jsx
@@ -1,6 +1,36 @@
 import React, { useState } from "react";
 import Join from "../../../components/Auth/Join/Join";
+import axios from "axios";
 
 export default function JoinContainer({ server }) {
-    return <Join />;
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        password: "",
+        chkPwd: "",
+        intro: "",
+    });
+    const onChange = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const { name, email, password, intro } = state;
+    const onSubmit = async () => {
+        console.log(name, email, password, intro);
+        try {
+            const joinInfo = await axios.post(`${server}/register`, {
+                name: name,
+                email: email,
+                password: password,
+                intro: intro,
+            });
+            console.log(joinInfo);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    return <Join state={state} onChange={onChange} onSubmit={onSubmit} />;
 }
